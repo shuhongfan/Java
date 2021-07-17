9404549087a6b0c398b20c7cddad0da716433f90
shuhongfan <shuhongfan@live.com>
2021-07-17 23:08:30 +0800

    2021.7.17 end

--- myByteStream/src/com/shf/FileOutputStreamdemo02.java
+++ myByteStream/src/com/shf/FileOutputStreamdemo02.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileOutputStreamdemo02 {
   public static void main(String[] args) throws IOException {
@@ -18,7 +19,11 @@ public class FileOutputStreamdemo02 {
     fos.write(100);
     fos.write(101);
 
-    byte[] bys={97,98,99,100,101};
-    fos.write(bys);
+//    byte[] bys={97,98,99,100,101};
+    byte[] bys = "abcde".getBytes(StandardCharsets.UTF_8);
+//    fos.write(bys);
+
+    fos.write(bys,1,3);
+    fos.close();
   }
 }
